e88975cc41fe12273f44cc3f33c8af33d0165f0a
周晓瑞 <zhouxiaorui@duiba.com.cn>
2019-05-15 16:19:46 +0800

    feat: 增加方法一

--- ZZTuYaDemo/AppDelegate.m
+++ ZZTuYaDemo/AppDelegate.m
@@ -17,9 +17,18 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+
+
+    NSLog(@"增加方法一");
+
+
     return YES;
 }
 
+
+- (void)addMethod1{
+
+}
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
